363194efad7a81001f809f14edbcb4c1f5e5fc8f
youassbee <selutin.anton@yandex.ru>
2026-05-24 20:55:25 +0300

    Добавил решение к заданию 1

---
+++ Arduino/Lesson_1/Software debounce/src/main.cpp
@@ -0,0 +1,52 @@
+#include <Arduino.h>
+#include "RotaryEncoder.h"
+
+#define encPin1 21
+#define encPin2 22
+#define button 8
+#define led 12
+
+RotaryEncoder encoder(encPin1, encPin2, RotaryEncoder::LatchMode::FOUR3);
+
+static int pos = 0;
+int newPos;
+
+static int buttonState = 1;
+int newButtonState;
+
+void setup() {
+  pinMode(button, INPUT_PULLUP);
+  pinMode(led, OUTPUT);
+  
+  Serial.begin(115200);
+  Serial.println(pos);
+}
+
+void loop() {
+  encoder.tick();
+  newPos = encoder.getPosition();
+
+  if(pos != newPos){
+    Serial.print(" pos:");
+    Serial.print(newPos);
+    Serial.print(" dir:");
+    Serial.print((int)(encoder.getDirection()));
+    Serial.print("\n");
+
+    pos = newPos;
+  }
+  
+  newButtonState = digitalRead(button);
+  if (buttonState != newButtonState) {
+    delay (300);
+    buttonState = newButtonState;
+    if (buttonState == LOW) {
+      digitalWrite(led, HIGH);
+      Serial.println("Кнопка нажата!");
+    } else {
+      digitalWrite(led, LOW);
+      Serial.println("Кнопка отпущена!");
+    }
+  }
+  
+}
